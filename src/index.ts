bb97e5453bad18769064a249a88535ad0496c5c9
unknown <miguel.semedo.2@outlook.com>
2022-12-06 16:26:32 +0000

    check number of wins of Man United

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,21 @@ const matches = fs
 	.map((row: string): string[] => {
 		return row.split(',');
 	});
+
+const MatchResult = {
+	HomeWin: 'H',
+	AwayWin: 'A',
+	Draw: 'D',
+};
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+	if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
+		manUnitedWins++;
+	} else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
+		manUnitedWins++;
+	}
+}
+
+console.log(`man united won: ${manUnitedWins} games`);
