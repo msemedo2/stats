7f928e7a69e9ec35181c866fb41957d789271725
unknown <miguel.semedo.2@outlook.com>
2022-12-07 11:15:02 +0000

    generate html report

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,12 @@ import { CsvFileReader } from './CsvFileReader';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
 import { WinsAnalyser } from './analysers/WinsAnalysis';
 import { Summary } from './Summary';
+import { HtmlReport } from './reportTargets/HtmlReport';
 
 const csvFileReader = new CsvFileReader('football.csv');
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const summary = new Summary(
-	new WinsAnalyser('Man United'),
-	new ConsoleReport()
-);
+const summary = new Summary(new WinsAnalyser('Man United'), new HtmlReport());
 summary.buildAndPrintReport(matchReader.matches);
