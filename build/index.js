4a368889f6501c58df6330047210eec85341680e
unknown <miguel.semedo.2@outlook.com>
2022-12-07 11:05:42 +0000

    build new summary report

--- build/index.js
+++ build/index.js
@@ -1,18 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
-const MatchResult_1 = require("./MatchResult");
 const CsvFileReader_1 = require("./CsvFileReader");
+const ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+const WinsAnalysis_1 = require("./analysers/WinsAnalysis");
+const Summary_1 = require("./Summary");
 const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 const matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-let manUnitedWins = 0;
-for (let match of matchReader.matches) {
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(`man united won: ${manUnitedWins} games`);
+const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalyser('Man United'), new ConsoleReport_1.ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResult';
 import { CsvFileReader } from './CsvFileReader';
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalyser } from './analysers/WinsAnalysis';
+import { Summary } from './Summary';
 
 const csvFileReader = new CsvFileReader('football.csv');
+
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
+
+const summary = new Summary(
+	new WinsAnalyser('Man United'),
+	new ConsoleReport()
+);
+summary.buildAndPrintReport(matchReader.matches);
